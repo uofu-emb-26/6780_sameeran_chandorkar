80e92aed96eafe377127aa4ce6a903783370ca8a
Sameeran <65390381+sameeran-c@users.noreply.github.com>
2026-02-26 02:13:28 -0700

    Lab 4 Checkoff 1

---
+++ lab4/Src/hal_gpio.c
@@ -0,0 +1,154 @@
+#include "hal_gpio.h"
+
+void GPIO_Init(GPIO_TypeDef *GPIOx, uint32_t pin, uint32_t mode, uint32_t speed,
+               uint32_t pull) {
+  uint32_t position = pin * 2; // Position for 2-bit fields
+
+  // Extract the actual mode and output type from the combined mode parameter
+  uint32_t gpio_mode = (mode & GPIO_MODE) >> GPIO_MODE_Pos;       // Bits 0-1
+  uint32_t output_type = (mode & OUTPUT_TYPE) >> OUTPUT_TYPE_Pos; // Bit 4
+
+  // Configure Mode, 2 bits per pin
+  GPIOx->MODER &= ~(0x3UL << position);
+  GPIOx->MODER |= (gpio_mode << position);
+
+  // Configure Output Type (only relevant for output modes)
+  if (gpio_mode == GPIO_MODE_OUTPUT_PP || gpio_mode == GPIO_MODE_AF_PP ||
+      gpio_mode == GPIO_MODE_OUTPUT_OD || gpio_mode == GPIO_MODE_AF_OD) {
+    GPIOx->OTYPER &= ~(0x1UL << pin);
+    GPIOx->OTYPER |= (output_type << pin);
+  }
+
+  // Configure Speed, 2 bits per pin
+  GPIOx->OSPEEDR &= ~(0x3UL << position);
+  GPIOx->OSPEEDR |= (speed << position);
+
+  // Configure pull-up/pull-down, 2 bits per pin
+  GPIOx->PUPDR &= ~(0x3UL << position);
+  GPIOx->PUPDR |= (pull << position);
+}
+
+void GPIO_DeInit(GPIO_TypeDef *GPIOx, uint32_t pin) {
+  // Reset logic can be implemented here if needed
+  // Typically involves resetting registers to default values
+}
+
+uint32_t GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint32_t pin) {
+  if (GPIOx->IDR & (0x1UL << pin)) {
+    return GPIO_PIN_SET;
+  }
+  return GPIO_PIN_RESET;
+}
+
+void GPIO_WritePin(GPIO_TypeDef *GPIOx, uint32_t pin, uint32_t state) {
+  if (state == GPIO_PIN_SET) {
+    GPIOx->BSRR = (0x1UL << pin); // Lower 16 bits to SET
+  } else {
+    GPIOx->BSRR = (0x1UL << (pin + 16)); // Upper 16 bits to RESET
+  }
+}
+
+/**
+ * @brief
+ */
+void GPIO_TogglePin(GPIO_TypeDef *GPIOx, uint32_t pin) {
+  GPIOx->ODR ^= (0x1UL << pin);
+}
+
+/**
+ * @brief Helper to calculate port index (A=0, B=1, etc.) based on memory
+ * address. Assumes ports are spaced by 0x400 bytes, which is standard for
+ * STM32F0
+ */
+static inline uint32_t GPIO_GetPortIndex(GPIO_TypeDef *port) {
+  return ((uint32_t)port - (uint32_t)GPIOA) / 0x400;
+}
+
+/**
+ * @brief Enable GPIO clock in RCC
+ */
+void GPIO_EnableClock(GPIO_TypeDef *GPIOx) {
+  // On STM32F0, GPIO clock enable bits in AHBENR start at bit 17 (GPIOA)
+  // and are contiguous for A, B, C, D, E, F.
+  // GPIOA_EN = Bit 17
+  // GPIOB_EN = Bit 18
+  // ...
+  uint32_t port_idx = GPIO_GetPortIndex(GPIOx);
+
+  // 17 is the bit position for GPIOAEN in RCC->AHBENR
+  RCC->AHBENR |= (1UL << (17 + port_idx));
+}
+
+/**
+ * @brief Set alternate function for a pin
+ */
+void GPIO_SetAlternateFunction(GPIO_TypeDef *GPIOx, uint32_t pin, uint32_t af) {
+  // AFR[0] = AFRL for pins 0-7
+  // AFR[1] = AFRH for pins 8-15
+  // Each pin uses 4 bits
+
+  uint32_t regIdx = pin / 8;       // 0 for pins 0-7, 1 for pins 8-15
+  uint32_t bitPos = (pin % 8) * 4; // Position within the register
+
+  GPIOx->AFR[regIdx] &= ~(0xF << bitPos); // Clear the 4 AF bits
+  GPIOx->AFR[regIdx] |= (af << bitPos);   // Set the AF number
+}
+
+/**
+ * @brief Configure EXTI for external interrupt on a pin
+ */
+void EXTI_Config(GPIO_TypeDef *GPIOx, uint32_t pin, uint32_t rising_edge,
+                 uint32_t falling_edge) {
+  // Determine port number
+  uint32_t port_index = GPIO_GetPortIndex(GPIOx);
+
+  // Enable SYSCFG clock
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+
+  // Configure SYSCFG EXTICR to route pin to EXTI
+  // EXTICR[0] = pins 0-3, EXTICR[1] = pins 4-7, etc.
+  uint32_t register_index = pin / 4;
+  uint32_t bit_position = (pin % 4) * 4;
+
+  SYSCFG->EXTICR[register_index] &= ~(0xFUL << bit_position);     // Clear
+  SYSCFG->EXTICR[register_index] |= (port_index << bit_position); // Set port
+
+  // Configure trigger edges
+  if (rising_edge)
+    EXTI->RTSR |= (1UL << pin);
+  else
+    EXTI->RTSR &= ~(1UL << pin);
+
+  if (falling_edge)
+    EXTI->FTSR |= (1UL << pin);
+  else
+    EXTI->FTSR &= ~(1UL << pin);
+
+  // Unmask/enable interrupt on this line
+  EXTI->IMR |= (1UL << pin);
+}
+
+/**
+ * @brief Enable EXTI interrupt in NVIC
+ */
+void EXTI_EnableInterrupt(uint32_t pin, uint32_t priority) {
+  IRQn_Type irq_number;
+
+  // Map EXTI line to NVIC IRQ
+  if (pin <= 1)
+    irq_number = EXTI0_1_IRQn;
+  else if (pin <= 3)
+    irq_number = EXTI2_3_IRQn;
+  else
+    irq_number = EXTI4_15_IRQn;
+
+  NVIC_SetPriority(irq_number, priority);
+  NVIC_EnableIRQ(irq_number);
+}
+
+/**
+ * @brief Clear EXTI pending flag (call this in your ISR!)
+ */
+void EXTI_ClearPending(uint32_t pin) {
+  EXTI->PR = (1UL << pin); // Write 1 to clear
+}
--- lab4/Src/lab4.c
+++ lab4/Src/lab4.c
@@ -1,85 +1,244 @@
+#include "hal_gpio.h"
 #include "main.h"
 #include "stm32f0xx_hal.h"
 
+#define LED_RED_PIN 6    // PC6
+#define LED_BLUE_PIN 7   // PC7
+#define LED_ORANGE_PIN 8 // PC8
+#define LED_GREEN_PIN 9  // PC9
+
+#define USART3_TX_PIN 10
+#define USART3_RX_PIN 11
+#define USART3_PORT GPIOC
+#define USART3_AF 1 // Alternate Function 1 for USART3
+
 void SystemClock_Config(void);
+void init_leds(void);
+void USART3_Init(void);
+void USART3_TransmitChar(char c);
+void USART3_TransmitString(const char *str);
+char USART3_ReceiveChar(void);
+uint32_t GetLedPin(char color);
+void PrintMenu(void);
+void ProcessCommand(char c);
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
   /* Configure the system clock */
   SystemClock_Config();
+  init_leds();
+  USART3_Init();
 
-  while (1)
-  {
- 
+  PrintMenu();
+
+  while (1) {
+    char c = USART3_ReceiveChar();
+    ProcessCommand(c);
   }
-  return -1;
+  return 0;
+}
+
+void init_leds(void) {
+  GPIO_EnableClock(GPIOC);
+
+  GPIO_Init(GPIOC, LED_RED_PIN, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW,
+            GPIO_NOPULL);
+  GPIO_Init(GPIOC, LED_BLUE_PIN, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW,
+            GPIO_NOPULL);
+  GPIO_Init(GPIOC, LED_ORANGE_PIN, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW,
+            GPIO_NOPULL);
+  GPIO_Init(GPIOC, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP, GPIO_SPEED_FREQ_LOW,
+            GPIO_NOPULL);
+}
+
+void USART3_Init(void) {
+  // Enable GPIO clock for USART pins
+  GPIO_EnableClock(USART3_PORT);
+
+  // Enable USART3 clock in RCC
+  RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
+  //__HAL_RCC_USART3_CLK_ENABLE();
+
+  // Configure GPIO pins for alternate function
+  GPIO_Init(USART3_PORT, USART3_TX_PIN, GPIO_MODE_AF_PP, GPIO_SPEED_FREQ_HIGH,
+            GPIO_NOPULL);
+  GPIO_Init(USART3_PORT, USART3_RX_PIN, GPIO_MODE_AF_PP, GPIO_SPEED_FREQ_HIGH,
+            GPIO_NOPULL);
+
+  // Set alternate function number (AF1 for USART3 on PC10/PC11)
+  GPIO_SetAlternateFunction(USART3_PORT, USART3_TX_PIN, USART3_AF);
+  GPIO_SetAlternateFunction(USART3_PORT, USART3_RX_PIN, USART3_AF);
+
+  // Configure baud rate (115200)
+  //  BRR = fCLK / baud_rate
+  USART3->BRR = HAL_RCC_GetHCLKFreq() / 115200;
+
+  // Enable transmitter (TE) and receiver (RE) in CR1
+  USART3->CR1 |= USART_CR1_TE | USART_CR1_RE;
+
+  // Enable USART peripheral
+  USART3->CR1 |= USART_CR1_UE;
+}
+
+void USART3_TransmitChar(char c) {
+  // Wait until TXE (Transmit Data Register Empty) flag is set
+  while (!(USART3->ISR & USART_ISR_TXE))
+    ;
+
+  // Write character to transmit data register
+  USART3->TDR = c;
+}
+
+void USART3_TransmitString(const char *str) {
+  while (*str != '\0') {
+    USART3_TransmitChar(*str);
+    str++;
+  }
+}
+
+char USART3_ReceiveChar(void) {
+  // Wait until RXNE (Receive Data Register Not Empty) flag is set
+  while (!(USART3->ISR & USART_ISR_RXNE))
+    ;
+
+  // Read and return received character (reading RDR clears RXNE)
+  return (char)(USART3->RDR);
+}
+
+uint32_t GetLedPin(char color) {
+  switch (color) {
+  case 'r':
+  case 'R':
+    return LED_RED_PIN;
+  case 'g':
+  case 'G':
+    return LED_GREEN_PIN;
+  case 'b':
+  case 'B':
+    return LED_BLUE_PIN;
+  case 'o':
+  case 'O':
+    return LED_ORANGE_PIN;
+  default:
+    return 0xFF; // Invalid
+  }
+}
+
+void PrintMenu(void) {
+  USART3_TransmitString("\r\n=== LED Control ===\r\n");
+  USART3_TransmitString("Press r/g/b/o to toggle LEDs\r\n\r\n");
+}
+
+void ProcessCommand(char c) {
+  // Ignore whitespace characters (CR, LF, Space, Tab)
+  if (c == '\r' || c == '\n' || c == ' ' || c == '\t') {
+    return;
+  }
+
+  uint32_t led_pin = GetLedPin(c);
+
+  if (led_pin != 0xFF) {
+    // Valid LED color, toggle it
+    GPIO_TogglePin(GPIOC, led_pin);
+
+    const char *color_name = "";
+    switch (c) {
+    case 'r':
+    case 'R':
+      color_name = "Red";
+      break;
+    case 'g':
+    case 'G':
+      color_name = "Green";
+      break;
+    case 'b':
+    case 'B':
+      color_name = "Blue";
+      break;
+    case 'o':
+    case 'O':
+      color_name = "Orange";
+      break;
+    }
+
+    USART3_TransmitString(color_name);
+    USART3_TransmitString(" LED toggled. State: ");
+
+    // Read the pin state to confirm (GPIO_ReadPin reads IDR)
+    if (GPIO_ReadPin(GPIOC, led_pin) == GPIO_PIN_SET) {
+      USART3_TransmitString("ON\r\n");
+    } else {
+      USART3_TransmitString("OFF\r\n");
+    }
+  } else {
+    // Invalid character - print error
+    USART3_TransmitString("Error: Unknown command '");
+    USART3_TransmitChar(c);
+    USART3_TransmitString("'\r\n");
+  }
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
+   */
+  RCC_ClkInitStruct.ClockType =
+      RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* User can add their own implementation to report the HAL error return state */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+  /* User can add their own implementation to report the HAL error return state
+   */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
 }
 
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* User can add their own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+  /* User can add their own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
 }
 #endif /* USE_FULL_ASSERT */
