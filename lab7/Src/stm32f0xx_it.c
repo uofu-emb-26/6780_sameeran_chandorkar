0a23e844fa089a6bbc98fab8f6eefb0666b7d296
Sameeran <sameeran-c@users.noreply.github.com>
2026-04-02 15:14:47 -0600

    Lab 7 intitial code

--- lab7/Src/stm32f0xx_it.c
+++ lab7/Src/stm32f0xx_it.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "stm32f0xx_hal.h"
 #include "stm32f0xx_it.h"
+#include "motor.h"
 
 /******************************************************************************/
 /*           Cortex-M0 Processor Interruption and Exception Handlers          */
@@ -45,6 +46,7 @@ void PendSV_Handler(void)
 void SysTick_Handler(void)
 {
   HAL_IncTick();
+  Lab7_SysTick_Handler();
 }
 
 /******************************************************************************/
